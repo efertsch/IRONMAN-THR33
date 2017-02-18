2e0cb9799f1fff2600a5f12c11b949cbdca551e4
Ethan Fertsch <efertsch@gmail.com>
2017-02-18 15:49:28 -0800

    creates seed file and formats round show page table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
 
 Deck.destroy_all
-
-foods_details = { :description    => "Foods That Make Your Stomach Gurggle"}
-
-crepe_details = { :description    => "That Crepe I At..."}
-
-fanny_details = { :description    => "Name That Fanny"}
+User.destroy_all
+Card.destroy_all
+Round.destroy_all
 
 
-Deck.create(foods_details)
-Deck.create(crepe_details)
-Deck.create(fanny_details)
+5.times do |i|
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password: "password")
+  Deck.create(description: Faker::Hipster.sentence)
+    5.times do
+      Card.create(question: Faker::Hipster.sentence, answer: Faker::Hipster.word, deck_id: i + 1)
+    end
+  Round.create(user_id: i + 1, deck_id: i + 1 , score: 0, maximum_potential_score: 5 )
+end
