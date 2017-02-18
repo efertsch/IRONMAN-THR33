0c69dda738410f2fa60b4f58f06128f9d3de0687
Christopher Huynh <email": null>
2017-02-17 18:04:17 -0800

    fixed the rest of user redirects, user forms work

--- app/controllers/sessions._controller.rb
+++ app/controllers/sessions._controller.rb
@@ -2,17 +2,17 @@ get '/decks/sessions/new' do
 	erb :'sessions/new'
 end
 
-post 'decks/sessions' do
+post '/decks/sessions' do
 	@user = User.authenticate(params[:user][:email], params[:user][:password])
 	if @user
 		login
-		redirect "decks/users/#{@user.id}"
+		redirect "/decks/users/#{@user.id}"
 	else
 		erb :'/sessions/new'
 	end
 end
 
-delete 'decks/sessions' do
+delete '/decks/sessions' do
 	logout
 	redirect '/'
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,29 +8,38 @@ get '/decks/users/new' do
 	erb :'/users/new'
 end
 
-post 'decks/users' do
+post '/decks/users' do
 	@user = User.new(params[:user])
 	if @user.save
-		redirect "decks/users/#{@user.id}"
+		redirect "/decks/users/#{@user.id}"
 	else
 		erb :'/users/new'
 	end
 end
 
-get 'decks/users/:id' do
-	erb :id
+get '/decks/users/:id' do
+	@user = User.find_by(id: params[:id])
+	erb :'users/show'
 end
 
 
-get 'decks/users/:id/edit' do
-
+get '/decks/users/:id/edit' do
+	@user = User.find_by(id: params[:id])
+	erb :'/users/edit'
 end
 
-put 'decks/users/:id' do
-
+put '/decks/users/:id' do
+	@user = User.find_by(id: params[:id])
+	@user.assign_attributes(params[:user])
+	if @user.save
+		redirect '/users'
+	else
+		erb :'/users/edit'
+	end
 end
 
-delete 'decks/users/:id' do
-
+delete '/decks/users/:id' do
+	@user.destroy
+	redirect '/'
 end
 
