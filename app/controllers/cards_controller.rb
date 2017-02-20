0d794b5836d5e4261e56226d5ea6813302cb9223
Ethan Fertsch <efertsch@gmail.com>
2017-02-19 16:44:49 -0800

    adds iterative card functionality to deck

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,14 +1,25 @@
-get '/cards' do
-	@cards = Card.all
-	erb :'cards/'
-end
-
+# get '/decks/:deck_id/cards' do
+# 	@cards = Card.all
+#   @first_card = @cards.first
+# 	erb :'cards/index'
+# end
 
 get '/decks/:deck_id/cards/:id' do
-  @first_card = Card.find_by(id: 1)
+  @deck = Deck.find_by(id: params[:deck_id])
 	@card = Card.find_by(id: params[:id])
-  @next_card = @card.id + 1
-	redirect "/decks/:deck_id/cards/#{@next_card}"
+  # @next_card = @card.id + 1
+  erb :'/cards/show'
+	# redirect "/decks/#{@deck.id}/cards/#{@next_card}"
 end
 
-
+post '/decks/:deck_id/cards/:id' do
+  @deck = Deck.find_by(id: params[:deck_id])
+  @card = Card.find_by(id: params[:id])
+  @last_card = @deck.cards.last
+  @next_card = @card.id + 1
+  if @card.id != @last_card.id
+    redirect "/decks/#{@deck.id}/cards/#{@next_card}"
+  else
+    redirect '/'
+  end
+end
